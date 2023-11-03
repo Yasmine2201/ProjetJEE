ef563a9a924e25b9dfc27218285d4650dc3dc679
ThibClnt <112936057+ThibClnt@users.noreply.github.com>
2023-11-03 23:57:58 +0100

    Non fonctionnel : erreur non résolue

---
+++ src/main/java/fr/efrei/teachfinder/rest/SchoolsResource.java
@@ -0,0 +1,23 @@
+package fr.efrei.teachfinder.rest;
+
+import fr.efrei.teachfinder.entities.School;
+import fr.efrei.teachfinder.services.ISchoolService;
+import jakarta.ejb.EJB;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("schools")
+public class SchoolsResource {
+    @EJB
+    private ISchoolService schoolService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<School> getSchools() {
+        return schoolService.getAllSchools();
+    }
+}
