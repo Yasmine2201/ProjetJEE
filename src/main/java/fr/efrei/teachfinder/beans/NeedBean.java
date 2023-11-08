df2f8597582c361f575f8ea26f48e752c071a225
ThibClnt <112936057+ThibClnt@users.noreply.github.com>
2023-11-09 00:39:50 +0100

    add NeedBean

---
+++ src/main/java/fr/efrei/teachfinder/beans/NeedBean.java
@@ -0,0 +1,79 @@
+package fr.efrei.teachfinder.beans;
+
+public class NeedBean {
+
+    private Integer needId;
+    private Integer recruiter;
+    private String schoolName;
+    private String subject;
+    private String contractType;
+    private String requirements;
+    private String timePeriod;
+    private String notes;
+
+    public NeedBean () { }
+
+    public Integer getNeedId() {
+        return needId;
+    }
+
+    public void setNeedId(Integer needId) {
+        this.needId = needId;
+    }
+
+    public Integer getRecruiter() {
+        return recruiter;
+    }
+
+    public void setRecruiter(Integer recruiter) {
+        this.recruiter = recruiter;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public String getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(String requirements) {
+        this.requirements = requirements;
+    }
+
+    public String getTimePeriod() {
+        return timePeriod;
+    }
+
+    public void setTimePeriod(String timePeriod) {
+        this.timePeriod = timePeriod;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
